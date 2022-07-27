409da4536ce8232430e1d78bf4507b1ef64b13d1
mojiSalehi <moji.salehif15@gmail.com>
2022-07-27 13:40:39 +0430

    index.js removed

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ let datas = []
 fetch('https://jsonplaceholder.typicode.com/posts').then(response => response.json()).then(d=>datas.push(d));
     
 let a = document.createElement('div')
-let nod = document.createElement('li');
+let nod = document.createElement('li')
